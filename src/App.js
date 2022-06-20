8cfd682d56058bf430c2ee99cb34e07011609b1d
joysen <joysenr96@gmail.com>
2022-06-20 11:29:51 -0700

    textt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> my app</h1>
+      <h3></h3>
+    </div>
+  );
+}
+
+export default App;
